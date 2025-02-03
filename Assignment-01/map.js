6ae0acd8b4925a10d2209aaaf2a06784f04b129b
SallyXiee <xx349@cornell.edu>
2025-02-02 19:27:00 -0500

    Initial commit

---
+++ Assignment-01/map.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoic2FsbHl4aWUwMSIsImEiOiJjbTZpOHNwbDkwNjQ0Mm1xMzc5YThrMGdiIn0.OXLCwLgEIO7lKMpqekNKlQ';
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/sallyxie01/cm6jjlhxc004m01s72knvccpy',
+    zoom: 10,
+    center: [-74, 40.725]
+});
---
+++ README.md
@@ -0,0 +1,3 @@
+# Repository for my projects
+
+This repository was built for the class.
